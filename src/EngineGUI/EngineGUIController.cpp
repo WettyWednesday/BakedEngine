b49cc671ef38d59a6100da7d753e65de7fe95af4
Mustafa <s235092@dtu.dk>
2024-12-28 23:41:26 +0100

    Started work on a GUI interface for the engine

---
+++ src/EngineGUI/EngineGUIController.cpp
@@ -0,0 +1,18 @@
+//
+// Created by magnu on 28-12-2024.
+//
+
+#include "EngineGUIController.h"
+
+void EngineGUIController::loadEngine()
+{
+    SetConfigFlags(FLAG_WINDOW_TOPMOST);
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "BakedEngine GUI");
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        EndDrawing();
+    }
+    CloseWindow();
+}
---
+++ src/EngineGUI/EngineGUIController.h
@@ -0,0 +1,25 @@
+//
+// Created by magnu on 28-12-2024.
+//
+
+#ifndef ENGINEGUICONTROLLER_H
+#define ENGINEGUICONTROLLER_H
+#include <cstdint>
+#include <string>
+
+
+#include "raylib.h"
+
+
+class EngineGUIController {
+public:
+    static void loadEngine();
+
+private:
+    constexpr static uint16_t SCREEN_WIDTH = 1080;
+    constexpr static uint16_t SCREEN_HEIGHT = 810;
+};
+
+
+
+#endif //ENGINEGUICONTROLLER_H
