3ace507dc87497bd81bed3b5ead9030a63d0d675
Mustafa <s235092@dtu.dk>
2024-12-29 17:55:39 +0100

    Playing around with texture loading. Not very functional right now

--- src/EngineGUI/EngineGUIController.cpp
+++ src/EngineGUI/EngineGUIController.cpp
@@ -6,46 +6,28 @@
 
 #include <iostream>
 
-#include "../TexturesManager/Tile.h"
-
+#include "../TexturesManager/TextureManager.h"
 
 void EngineGUIController::loadEngine()
 {
     SetConfigFlags(FLAG_WINDOW_TOPMOST);
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "BakedEngine GUI");
-    Tile tile1;
-    Tile tile2;
-    tile1.setNewSize(200, 200, true);
 
-    tile2.setNewSize(200, 200, true);
-    tile2.setNewPosition(tile1.getPosition().first + tile1.getSize().first,
-                         tile1.getPosition().second);
-    tile2.setNewTexture("Grass");
+    TextureManager TM;
+    B_Texture b1;
+    Tile t1;
+    t1.setNewTexture("Grass");
+    b1.textureTile = t1;
+    b1.color = WHITE;
+    TM.textureInit(b1);
 
-    Texture2D texture1 = LoadTexture(tile1.getTexture().c_str());
-    texture1.width = tile1.getSize().first;
-    texture1.height = tile1.getSize().second;
-
-    Texture2D texture2 = LoadTexture(tile2.getTexture().c_str());
-    texture2.width = tile2.getSize().first;
-    texture2.height = tile2.getSize().second;
-
-    if (texture1.id == 0)
-    {
-        std::cerr << "Failed to load texture!" << std::endl;
-        return;
-    }
-    if (texture2.id == 0)
-    {
-        std::cerr << "Failed to load texture!" << std::endl;
-        return;
-    }
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawTexture(texture1, tile1.getPosition().first, tile1.getPosition().second, WHITE);
-        DrawTexture(texture2, tile2.getPosition().first, tile2.getPosition().second, WHITE);
+
+        TM.loadAllTextures();
+        DrawFPS(SCREEN_WIDTH, 0);
         EndDrawing();
     }
     CloseWindow();
---
+++ src/TexturesManager/TextureManager.cpp
@@ -0,0 +1,35 @@
+//
+// Created by magnu on 29-12-2024.
+//
+
+#include "TextureManager.h"
+
+#include <iostream>
+
+void TextureManager::loadAllTextures()
+{
+    if (!displayedTextures.empty())
+    {
+        if (!textureLoaded)
+        {
+            for (B_Texture &b_Text : displayedTextures)
+            {
+
+                Texture2D txt = LoadTexture(b_Text.textureTile.getTexture().c_str());
+                if (txt.id == 0)
+                {
+                    std::cerr << "Failed to load texture: " << b_Text.textureTile.getTexture() << std::endl;
+                    continue;
+                }
+
+                DrawTexture(txt, b_Text.textureTile.getPosition().first, b_Text.textureTile.getPosition().second,
+                            b_Text.color);
+            }
+            textureLoaded = true;
+        }
+    }
+    else
+    {
+        std::cerr << "Cant load textures, no textures to load!" << std::endl;
+    }
+}
