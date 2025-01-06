82ec8c22463f434a43c3419cb6162164233da949
Mustafa <s235092@dtu.dk>
2025-01-07 00:50:13 +0100

    My approach is as follows. Create a bunch of leaves, all deriving from a Base Leaf with some standard functionality like position fx. Later other functionalities can come, but for now this will suffice. And then everything will derive from this class. So that i can essentially create a single list of Base Leafs and loop them (maybe)

---
+++ src/BaseLeaf.h
@@ -0,0 +1,19 @@
+#ifndef BASELEAF_H
+#define BASELEAF_H
+
+class BaseLeaf
+{
+public:
+    virtual ~BaseLeaf() = default;
+
+protected:
+    virtual void setPosition(int _x, int _y);
+    virtual std::pair<int,int> getPosition();
+
+private:
+    int posX = 0;
+    int posY = 0;
+};
+
+
+#endif //BASELEAF_H
